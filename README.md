febba68222e20b8101708f0aecde2abe984484db
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-05-29 09:52:13 -0500

    Fix areas calculations.

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 Analysis & graphics illustrating Tyler's historical growth.
 
-## Generating GIF
+## Usage
 
-Run `animation.R` then:
+Generating GIF:
 
 ```
 convert -resize '1188x827>' -delay 100 -loop 0 animation/*.png animation/animation.gif
+```
+
+Generating TopoJSON:
+
+```
+topojson data/with_area/with_area.shp --out annexations.json --properties YEAR
 ```
 
 ## Sources
--- animation.R
+++ animation.R
@@ -18,25 +18,35 @@ ggplot() +
   coord_fixed(xlim = c(272000, 295000), ylim = c(3567000, 3595000)) +
   annotate("text", x = 275000, y = 3568000, label = "2017") + 
   theme_map() +
+  guides(fill = FALSE) + 
   ggsave("animation/2017.png")
 
 for (i in seq(1940, 2017, 10)) {
   print(i)
   tyler <- subset(df, YEAR < i)
   
-  tyler$addition <- (tyler$YEAR < i) && (tyler$YEAR >= i - 10)
+  tyler$addition <- tyler$YEAR >= i - 10
+
+  area <- tyler %>%
+    group_by(id) %>%
+    filter(row_number() == 1) %>%
+    ungroup() %>%
+    summarise(total = sum(AREA))
   
-  areas[nrow(areas) + 1, ] <- c(i, sum(tyler$AREA))
+  areas[nrow(areas) + 1, ] <- c(i, area$total)
   
   ggplot() +
     geom_polygon(data = tyler, aes(x = long, y = lat, group = group, fill = addition)) +
     coord_fixed(xlim = c(272000, 295000), ylim = c(3567000, 3595000)) +
     annotate("text", x = 275000, y = 3568000, label = i) + 
     theme_map() +
+    guides(fill = FALSE) + 
     ggsave(paste0("animation/", i, ".png"))
 }
+
+areas[nrow(areas) + 1, ] <- c(2017, sum(annexations@data$AREA))
 
 areas <- areas %>%
   mutate(area_sq_mi = area_sq_m * 0.0000003861022)
 
-write.csv(areas, "areas.csv", row.names = FALSE)
+write.csv(areas, "results/areas.csv", row.names = FALSE)
